d151fc7829510f306973da02b4abca069a89badf
beylax <manuel.baldoni04@gmail.com>
2022-01-28 19:59:37 +0100

    Prova negozio 3

--- src/Pagine/Negozio.js
+++ src/Pagine/Negozio.js
@@ -1,22 +1,56 @@
-import React from "react";
+import {useEffect, useRef} from 'react';
 
-class Negozio extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div className="Negozio">
-                <div className="container mt-3">
-                <div id="my-store-71128923"></div>
-                <div>
-                    <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?71128923&data_platform=code&data_date=2022-01-28" charset="utf-8"></script><script type="text/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-71128923");</script>
-                </div>
-                </div>
-            </div>
-        )
-    }
+function Negozio() {
+
+const storeDiv = useRef(null);
+const scriptRef = useRef(null);
+
+window.localStorage.setItem("show_ecwid_logs","true")
+window.ecwid_script_defer = true;
+window.ecwid_dynamic_widgets = true;
+window.ec = window.ec || Object();
+window.ec.storefront = window.ec.storefront || Object();
+window.ec.enable_catalog_on_one_page = true;
+window._xnext_initialization_scripts = [{
+  widgetType: 'ProductBrowser',
+  id: 'my-store-71128923',
+  arg: ["id=productBrowser", "views=grid(20,3)"]  },
+  {
+    widgetType: 'CategoriesV2',
+    id: 'my-categories-71128923',
+    arg: ["id=categoriesV2"]
+  }
+  ,
+  {
+    widgetType: 'SearchWidget',
+    id: 'my-search-71128923',
+    arg: ["id=searchWidget"]
+  }
+];
+
+
+
+let script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = ('https://app.ecwid.com/script.js?71128923&data_platform=code&data_date=2022-01-28');
+script.defer = true;
+script.ref=scriptRef;
+
+useEffect(() => {
+if(!scriptRef.current){
+  storeDiv.current.appendChild(script);
+   }
+});
+
+return (
+  <div className="store">
+        <div id="my-search-71128923"></div>
+        <div id="my-categories-71128923"></div>
+        <div id="my-store-71128923" ref={storeDiv}></div>
+        <div className="ec-cart-widget"></div>
+  </div>
+
+  );
 }
- 
-export default Negozio
+
+export default Negozio;
