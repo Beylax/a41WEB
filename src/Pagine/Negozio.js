1834e1545d6967efd535e1c1d0e2293bdaaa6261
beylax <manuel.baldoni04@gmail.com>
2022-01-28 18:43:23 +0100

    Seconda prova ecommerce

--- src/Pagine/Negozio.js
+++ src/Pagine/Negozio.js
@@ -10,19 +10,9 @@ class Negozio extends React.Component {
             <div className="Negozio">
                 <div className="container mt-3">
                 <div id="my-store-71128923"></div>
-                    <div>
-                        <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?71128923&data_platform=code&data_date=2022-01-28" charset="utf-8"></script><script type="text/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-71128923");</script>
-                    </div>
-                </div>
-                <div id="my-search-71128923"></div>
                 <div>
-                    <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?71128923&data_platform=code&data_date=2022-01-28" charset="utf-8"></script>
-                    <script type="text/javascript"> xSearch("id=my-search-71128923"); </script>
+                    <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?71128923&data_platform=code&data_date=2022-01-28" charset="utf-8"></script><script type="text/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-71128923");</script>
                 </div>
-                <div class="ec-cart-widget"></div>
-                <div>
-                    <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?71128923&data_platform=code&data_date=2022-01-28" charset="utf-8"></script>
-                    <script type="text/javascript">Ecwid.init();</script>
                 </div>
             </div>
         )
